af4c361f11e86d61dde4be9ee0d4f3ba15811fc5
Chris Randall <65134095+Cmrandall86@users.noreply.github.com>
2024-11-05 16:32:23 -0700

    updated schema simplified without images

--- schemaTypes/blogPost.js
+++ schemaTypes/blogPost.js
@@ -1,21 +1,19 @@
 export default {
-    name: 'blogPost',
-    title: 'Blog Post',
-    type: 'document',
-    fields: [
-      { name: 'title', title: 'Title', type: 'string' },
-      { name: 'description', title: 'Description', type: 'text' },
-      { name: 'productLinks', title: 'Product Links', type: 'url' },
-      { name: 'images', title: 'Images', type: 'array', of: [{ type: 'image' }] },
-      { name: 'price', title: 'Price', type: 'number' },
-      {
-        name: 'author',
-        title: 'Author',
-        type: 'reference',
-        to: [{ type: 'user' }],
-      },
-      { name: 'createdAt', title: 'Created At', type: 'datetime' },
-      { name: 'updatedAt', title: 'Updated At', type: 'datetime' },
-    ],
-  };
-  
+  name: 'blogPost',
+  title: 'Blog Post',
+  type: 'document',
+  fields: [
+    { name: 'title', title: 'Title', type: 'string' },
+    { name: 'description', title: 'Description', type: 'text' },
+    { name: 'productURL', title: 'Product URL', type: 'url' }, // Updated from 'link' to 'productURL'
+    { name: 'price', title: 'Price', type: 'number' },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: [{ type: 'user' }],
+    },
+    { name: 'createdAt', title: 'Created At', type: 'datetime' },
+    { name: 'updatedAt', title: 'Updated At', type: 'datetime' },
+  ],
+};
