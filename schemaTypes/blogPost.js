b42f8f3fc59aba5d24e53ff5043de7387341b3ec
Cmrandall86 <cmrandall86@gmail.com>
2024-11-07 17:02:26 -0700

    schema update

--- schemaTypes/blogPost.js
+++ schemaTypes/blogPost.js
@@ -1,19 +1,28 @@
 export default {
-  name: 'blogPost',
-  title: 'Blog Post',
-  type: 'document',
+  name: "blogPost",
+  title: "Blog Post",
+  type: "document",
   fields: [
-    { name: 'title', title: 'Title', type: 'string' },
-    { name: 'description', title: 'Description', type: 'text' },
-    { name: 'productURL', title: 'Product URL', type: 'url' }, // Updated from 'link' to 'productURL'
-    { name: 'price', title: 'Price', type: 'number' },
+    { name: "title", title: "Title", type: "string" },
+    { name: "description", title: "Description", type: "text" },
+    { name: "productURL", title: "Product URL", type: "url" },
+    { name: "price", title: "Price", type: "number" },
     {
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: [{ type: 'user' }],
+      name: "createdAt",
+      title: "Created At",
+      type: "datetime",
+      options: {
+        readOnly: true,
+      },
     },
-    { name: 'createdAt', title: 'Created At', type: 'datetime' },
-    { name: 'updatedAt', title: 'Updated At', type: 'datetime' },
+    {
+      name: "updatedAt",
+      title: "Updated At",
+      type: "datetime",
+      options: {
+        readOnly: true,
+      },
+    },
+    // Add other fields as needed
   ],
 };
